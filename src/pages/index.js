8bfeb7563606abc869b3c68e776248427581adff
Ian Young <66717+ianyoung@users.noreply.github.com>
2023-10-17 15:26:04 +0100

    Remove logic from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,6 @@
 import Link from 'next/link';
-import { gql, useQuery } from '@apollo/client';
-
-const GET_PROPERTIES = gql`
-    query GetProperties {
-        properties {
-            title
-        }
-    }
-`;
 
 export default function Home() {
-    const { loading, error, data } = useQuery(GET_PROPERTIES);
-
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${error.message}`;
-
     return (
         <main className="bg-slate-100 flex justify-center">
             <div className="w-3/6">
@@ -25,12 +11,12 @@ export default function Home() {
                 </div>
                 <div>
                     <ul>
-                        <Link href="/clientside">
+                        <Link href="/client-side">
                             <li className="bg-white rounded-md border border-slate-200/75 my-2 p-4 leading-normal">
                                 Client-side rendering
                             </li>
                         </Link>
-                        <Link href="/serverside">
+                        <Link href="/server-side">
                             <li className="bg-white rounded-md border border-slate-200/75 my-2 p-4 leading-normal">
                                 Server-side rendering
                             </li>
